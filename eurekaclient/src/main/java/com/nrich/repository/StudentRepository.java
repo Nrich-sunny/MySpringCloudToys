c4a9b2551b3d22a05587c34b367ceb13c013f006
N7301 <natatime@163.com>
2023-01-27 22:37:13 +0800

    :sparkles: 完成服务提供者提供的服务——学生信息的增删改查 这部分功能的实现即SpringBoot的正常流程

---
+++ eurekaclient/src/main/java/com/nrich/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.nrich.repository;
+
+import com.nrich.entity.Student;
+
+import java.util.Collection;
+
+public interface StudentRepository {
+    public Collection<Student> findAll();
+    public Student findById(long id);
+    public void saveOrUpdate(Student student);
+    public void deleteById(long id);
+}
